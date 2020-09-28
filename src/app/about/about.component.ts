cca432019a501b691aa22ec7fd069314732ef9c8
Felipe Chernicharo <string7dev@gmail.com>
2020-09-28 15:50:07 -0300

    prepara substituição firebaseSDK pelo AngularFire

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -4,18 +4,9 @@ import { Course } from "app/model/course";
 import * as firebase from "firebase/app";
 import "firebase/firestore";
 // import { firestore } from  'firebase'
+import { environment } from "../../environments/environment";
 
-const config = {
-  apiKey: "AIzaSyCPL_TTVytKOsPx1JNjk2gYKABQKjiwzvQ",
-  authDomain: "fire-course-app.firebaseapp.com",
-  databaseURL: "https://fire-course-app.firebaseio.com",
-  projectId: "fire-course-app",
-  storageBucket: "fire-course-app.appspot.com",
-  messagingSenderId: "746854701396",
-  appId: "1:746854701396:web:917a64184758b6fc5890c1",
-  measurementId: "G-9KCPSTL7NW",
-};
-firebase.initializeApp(config);
+firebase.initializeApp(environment.firebaseConfig);
 
 const db = firebase.firestore();
 
@@ -31,14 +22,6 @@ export class AboutComponent implements OnInit {
   constructor() {}
 
   ngOnInit() {
-    // db.doc("courses/R8mq7RDDirwcO6tg1Dt0")
-    //   .get()
-    //   .then((snap) => console.log(snap.data()));
-
-    // db.doc("/courses/R8mq7RDDirwcO6tg1Dt0/lessons/5e6fp67m9haBhoF3oLWu")
-    //   .get()
-    //   .then((snap) => console.log(snap.data()));
-
     db.collection("courses")
       .get()
       .then((snaps) => {
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,14 @@
 
 export const environment = {
   production: false,
-  firebase: {
-
-  }
+  firebaseConfig: {
+    apiKey: "AIzaSyCPL_TTVytKOsPx1JNjk2gYKABQKjiwzvQ",
+    authDomain: "fire-course-app.firebaseapp.com",
+    databaseURL: "https://fire-course-app.firebaseio.com",
+    projectId: "fire-course-app",
+    storageBucket: "fire-course-app.appspot.com",
+    messagingSenderId: "746854701396",
+    appId: "1:746854701396:web:917a64184758b6fc5890c1",
+    measurementId: "G-9KCPSTL7NW",
+  },
 };
