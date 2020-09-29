7e210a9da9d527b60e35f799f5dbf1698bc51a5a
Felipe Chernicharo <string7dev@gmail.com>
2020-09-29 19:11:25 -0300

    criando btns de signin com firebaseui

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,18 +1,33 @@
-import { Component, OnInit } from '@angular/core';
-import * as firebaseui from 'firebaseui';
-import * as firebase from 'firebase/app';
-
+import { Component, OnInit } from "@angular/core";
+import * as firebaseui from "firebaseui";
+import * as firebase from "firebase/app";
+import { AngularFireAuth } from "@angular/fire/auth";
 
 @Component({
-  selector: 'login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  selector: "login",
+  templateUrl: "./login.component.html",
+  styleUrls: ["./login.component.scss"],
 })
 export class LoginComponent implements OnInit {
+  ui: firebaseui.auth.AuthUI;
 
-  constructor() { }
+  constructor(private afAuth: AngularFireAuth) {}
 
   ngOnInit() {
+    const uiConfig: firebaseui.auth.Config = {
+      signInOptions: [
+        firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+        firebase.auth.EmailAuthProvider.PROVIDER_ID,
+      ],
+      callbacks: {
+        signInSuccessWithAuthResult: this.onLoginSuccess.bind(this),
+      },
+    };
+
+    this.ui = new firebaseui.auth.AuthUI(this.afAuth.auth);
+
+    this.ui.start("#firebaseui-auth-container", uiConfig);
   }
 
+  onLoginSuccess() {}
 }
