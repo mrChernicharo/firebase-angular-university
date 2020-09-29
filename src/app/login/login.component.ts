46694b964dce259e0979d33b858f54c0ccb9ed2a
Felipe Chernicharo <string7dev@gmail.com>
2020-09-29 20:18:39 -0300

    signin com fireAuth + ngZone

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,17 +1,22 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, NgZone, OnDestroy, OnInit } from "@angular/core";
 import * as firebaseui from "firebaseui";
 import * as firebase from "firebase/app";
 import { AngularFireAuth } from "@angular/fire/auth";
+import { Router } from "@angular/router";
 
 @Component({
   selector: "login",
   templateUrl: "./login.component.html",
   styleUrls: ["./login.component.scss"],
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent implements OnInit, OnDestroy {
   ui: firebaseui.auth.AuthUI;
 
-  constructor(private afAuth: AngularFireAuth) {}
+  constructor(
+    private afAuth: AngularFireAuth,
+    private router: Router,
+    private ngZone: NgZone
+  ) {}
 
   ngOnInit() {
     const uiConfig: firebaseui.auth.Config = {
@@ -29,5 +34,13 @@ export class LoginComponent implements OnInit {
     this.ui.start("#firebaseui-auth-container", uiConfig);
   }
 
-  onLoginSuccess() {}
+  ngOnDestroy() {
+    this.ui.delete();
+  }
+
+  onLoginSuccess(result) {
+    console.log("Firebase UI result: ", result);
+    this.router.navigateByUrl("/courses");
+    this.ngZone.run(() => this.router.navigateByUrl("/courses"));
+  }
 }
