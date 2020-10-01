7495b004b11b007b34cc75ae30a1cf98d82733de
Felipe Chernicharo <string7dev@gmail.com>
2020-09-30 22:42:24 -0300

    build + firebase deploy e hosting

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyCPL_TTVytKOsPx1JNjk2gYKABQKjiwzvQ",
+    authDomain: "fire-course-app.firebaseapp.com",
+    databaseURL: "https://fire-course-app.firebaseio.com",
+    projectId: "fire-course-app",
+    storageBucket: "fire-course-app.appspot.com",
+    messagingSenderId: "746854701396",
+    appId: "1:746854701396:web:917a64184758b6fc5890c1",
+    measurementId: "G-9KCPSTL7NW",
+  },
 };
