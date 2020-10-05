f40a732ed2b020948fdb3c36e017d34828bab562
Felipe Chernicharo <string7dev@gmail.com>
2020-10-05 20:59:12 -0300

    trigger onDeleteLesson

--- functions/src/lessons-counter.ts
+++ functions/src/lessons-counter.ts
@@ -8,23 +8,35 @@ export const onAddLesson = functions.firestore
 
     console.log("running onAddLesson trigger ...");
 
-<<<<<<< HEAD
-    return db.runTransaction(async (transaction) => {
-=======
-    db.runTransaction(async (transaction) => {
->>>>>>> 5a17faace04a02f529319bf86b3d0e6bea2ec602
-      const courseRef = snap.ref.parent
-        .parent as FirebaseFirestore.DocumentReference<
-        FirebaseFirestore.DocumentData
-      >;
-
-      const courseSnapshot = await transaction.get(courseRef);
-
-      const course = courseSnapshot.data();
-
-      if (course) {
-        const changes = { lessonsCount: +course.lessonsCount + 1 };
-        transaction.update(courseRef, changes);
-      }
+    return courseTransaction(snap, (course) => {
+      return { lessonsCount: +course.lessonsCount + 1 };
     });
   });
+
+export const onDeleteLesson = functions.firestore
+  .document("courses/{courseId}/lessons/{lessonId}")
+  .onDelete(async (snap, context) => {
+    console.log("running onAddLesson trigger ...");
+
+    return courseTransaction(snap, (course) => {
+      return { lessonsCount: +course.lessonsCount - 1 };
+    });
+  });
+
+function courseTransaction(snap, cb: Function) {
+  return db.runTransaction(async (transaction) => {
+    const courseRef = snap.ref.parent
+      .parent as FirebaseFirestore.DocumentReference<
+      FirebaseFirestore.DocumentData
+    >;
+
+    const courseSnapshot = await transaction.get(courseRef);
+
+    const course = courseSnapshot.data();
+
+    if (course) {
+      const changes = cb(course);
+      transaction.update(courseRef, changes);
+    }
+  });
+}
